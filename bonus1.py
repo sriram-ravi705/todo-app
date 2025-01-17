4a429e53da3d201074f38e732ac4518c58e10890
sriram-ravi705 <sriramravi477@gmail.com>
2025-01-17 22:32:00 +0530

    day-1

---
+++ bonus1.py
@@ -0,0 +1,2 @@
+book_title = input("Enter book title: ")
+print("Book title is", len(book_title))
---
+++ main.py
@@ -0,0 +1,7 @@
+val1 = input('Enter val1: ')
+val2 = input('Enter val2: ')
+val3 = input('Enter val3: ')
+user_detail = [val1, val2, val3]
+print(user_detail)
+
+print(type(user_detail))
