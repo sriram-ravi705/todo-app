e2911291e2d88bbe8dfb27f2d159c6e2b8551302
sriram-ravi705 <sriramravi477@gmail.com>
2025-01-20 17:09:09 +0530

    day-4

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 todo_list = []
 
 while True:
-    user_action = input("Type add. show, or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
 
     match user_action:
         case 'add':
             todo = input('Enter todo: ').strip()
             todo_list.append(todo)
         case 'show' | 'display':
-            for todo_items in todo_list:
-                print('-', todo_items.title())
+            for index, todo_items in enumerate(todo_list):
+                print(index + 1, '-', todo_items.title())
+        case 'edit':
+            number = int(input("number of todo to edit: "))
+            number = number - 1
+            new_todo = input('Enter new todo: ')
+            todo_list[number] = new_todo
+            print('Todo updated')
         case 'exit':
             break
         case _:
