6628dfa8d30de783455f3dc38a360416ee6e03a0
sriram-ravi705 <sriramravi477@gmail.com>
2025-01-24 09:58:35 +0530

    day-8

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# todo_list = []
+todo_list = []
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
@@ -6,36 +6,37 @@ while True:
     match user_action:
         case 'add':
             todo = input('Enter todo: ') + "\n"
-
-            file = open('files/todo', 'r')
-            todo_list = file.readlines()
-            file.close()
-
+            with open('files/todo', 'r') as file:
+                todo_list = file.readlines()
             todo_list.append(todo)
-
-            file = open('files/todo', 'w')
-            file.writelines(todo_list)
-            file.close()
+            with open('files/todo', 'w') as file:
+                file.writelines(todo_list)
         case 'show' | 'display':
-            file = open('files/todo', 'r')
-            todo_list = file.readlines()
-            file.close()
-
+            with open('files/todo', 'r') as file:
+                todo_list = file.readlines()
             for index, todo_items in enumerate(todo_list):
                 todo_items = todo_items.strip("\n")
-                print(type(todo_items))
                 print(f'{index + 1}-{todo_items.title()}')
             print(f'Total todo: {len(todo_list)}')
         case 'edit':
             number = int(input("number of todo to edit: "))
             number = number - 1
+            with open('files/todo', 'r') as file:
+                todo_list = file.readlines()
             new_todo = input('Enter new todo: ')
-            todo_list[number] = new_todo
+            todo_list[number] = new_todo + "\n"
+            with open('files/todo', 'w') as file:
+                file.writelines(todo_list)
             print('Todo updated')
         case 'complete':
             number = int(input("number of todo to completed: "))
+            with open('files/todo', 'r') as file:
+                todo_list = file.readlines()
+            removed_todo = todo_list[number - 1].strip("\n")
             todo_list.pop(number - 1)
-            print('Todo updated')
+            with open('files/todo', 'w') as file:
+                file.writelines(todo_list)
+            print(f'Todo {removed_todo} removed ')
         case 'exit':
             break
         case _:
