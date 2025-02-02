a23cd635c35289636f47a9b18babda34f3ede9ea
sriram-ravi705 <sriramravi477@gmail.com>
2025-02-02 14:46:48 +0530

    day-17

--- gui.py
+++ gui.py
@@ -5,6 +5,7 @@ label = FreeSimpleGUI.Text("Type in a to-do")
 input_boc = FreeSimpleGUI.InputText(tooltip="Enter todo", key="todo")
 add_button = FreeSimpleGUI.Button("Add")
 
+
 Window = FreeSimpleGUI.Window("My Gui Todo App", layout=[[label, input_boc], [add_button]],
                               font=('Helvetica', 20))
 
